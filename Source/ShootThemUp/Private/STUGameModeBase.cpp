c1b027f3b9339767d52edbbc379cd7e501c8414e
Dmitriy Smirnov <dsmirnov.online@yandex.ru>
2023-02-02 21:36:50 +0300

    Init commit

---
+++ Source/ShootThemUp/Private/STUGameModeBase.cpp
@@ -0,0 +1,13 @@
+// Shoot Them Up Game. All Rights Reserved.
+
+
+#include "STUGameModeBase.h"
+
+#include "STUBaseCharacter.h"
+#include "STUPlayerController.h"
+
+ASTUGameModeBase::ASTUGameModeBase()
+{
+	DefaultPawnClass = ASTUBaseCharacter::StaticClass();
+	PlayerControllerClass = ASTUPlayerController::StaticClass();
+}
