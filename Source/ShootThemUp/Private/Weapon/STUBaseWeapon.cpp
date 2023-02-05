94d3cc04da7e665105b9791a89ed49ce0c341f8d
Dmitriy Smirnov <dsmirnov.online@yandex.ru>
2023-02-05 17:04:42 +0300

    Rifle, Launcher controls

--- Source/ShootThemUp/Private/Weapon/STUBaseWeapon.cpp
+++ Source/ShootThemUp/Private/Weapon/STUBaseWeapon.cpp
@@ -25,35 +25,14 @@ void ASTUBaseWeapon::BeginPlay()
 
 void ASTUBaseWeapon::StartFire()
 {
-	MakeShot();
-	GetWorldTimerManager().SetTimer(ShotTimerHandle, this, &ASTUBaseWeapon::MakeShot, TimeBetweenShots, true);
 }
 
 void ASTUBaseWeapon::StopFire()
 {
-	GetWorldTimerManager().ClearTimer(ShotTimerHandle);
 }
 
 void ASTUBaseWeapon::MakeShot()
 {
-	if (!GetWorld()) return;
-
-	FVector TraceStart, TraceEnd;
-	if (!GetTraceData(TraceStart, TraceEnd)) return;
-
-	FHitResult HitResult;
-	MakeHit(HitResult, TraceStart, TraceEnd);
-
-	if (HitResult.bBlockingHit)
-	{
-		MakeDamage(HitResult);
-		DrawDebugLine(GetWorld(), GetMuzzleWorldLocation(), HitResult.ImpactPoint, FColor::Red, false, 3.0f, 0, 3.0f);
-		DrawDebugSphere(GetWorld(), HitResult.ImpactPoint, 10.0f, 24, FColor::Red, false, 5.0f);
-	} else
-	{
-		DrawDebugLine(GetWorld(), GetMuzzleWorldLocation(), TraceEnd, FColor::Red, false, 3.0f, 0, 3.0f);
-	}
-	
 }
 
 
@@ -85,9 +64,8 @@ bool ASTUBaseWeapon::GetTraceData(FVector& TraceStart, FVector& TraceEnd) const
 	FRotator ViewRotation;
 	if (!GetPlayerViewPoint(ViewLocation, ViewRotation)) return false;
 
-	TraceStart = ViewLocation; // GetMuzzleWorldLocation();
-	const auto HalfRad = FMath::DegreesToRadians(BulletSpread);
-	const FVector ShootDirection = FMath::VRandCone(ViewRotation.Vector(), HalfRad) ; // SocketTransform.GetRotation().GetForwardVector();
+	TraceStart = ViewLocation;
+	const FVector ShootDirection = ViewRotation.Vector();
 	TraceEnd = TraceStart + ShootDirection * TraceMaxDistance;
 
 	return true;
---
+++ Source/ShootThemUp/Private/Weapon/STULauncherWeapon.cpp
@@ -0,0 +1,23 @@
+// Shoot Them Up Game. All Rights Reserved.
+
+
+#include "Weapon/STULauncherWeapon.h"
+
+
+#include "Weapon/STUProjectile.h"
+#include "Kismet/GameplayStatics.h"
+
+void ASTULauncherWeapon::StartFire()
+{
+	MakeShot();
+}
+
+void ASTULauncherWeapon::MakeShot()
+{
+	const FTransform SpawnTransform(FRotator::ZeroRotator, GetMuzzleWorldLocation());
+	auto Projectile = UGameplayStatics::BeginDeferredActorSpawnFromClass(GetWorld(), ProjectileClass, SpawnTransform);
+	// Set projectile params
+
+	UGameplayStatics::FinishSpawningActor(Projectile, SpawnTransform);
+}
+
