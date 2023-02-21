8d8d7900abfba2369f161d83bc4e851ba0eb338f
Dmitriy Smirnov <dsmirnov.online@yandex.ru>
2023-02-21 21:34:33 +0300

    EQS only visible Pickups

--- Source/ShootThemUp/Private/Pickups/STUBasePickup.cpp
+++ Source/ShootThemUp/Private/Pickups/STUBasePickup.cpp
@@ -45,6 +45,11 @@ void ASTUBasePickup::Tick(float DeltaTime)
 	AddActorLocalRotation(FRotator(0.0f, RotationYaw, 0.0f));
 }
 
+bool ASTUBasePickup::CouldBeTaken() const
+{
+	return !GetWorldTimerManager().IsTimerActive(RespawnTimerHandle);
+}
+
 bool ASTUBasePickup::GivePickupTo(APawn* PlayerPawn)
 {
 	return false;
@@ -57,8 +62,7 @@ void ASTUBasePickup::PickupWasTaken()
 	{
 		GetRootComponent()->SetVisibility(false, true);
 	}
-
-	FTimerHandle RespawnTimerHandle;
+	
 	GetWorldTimerManager().SetTimer(RespawnTimerHandle, this,&ASTUBasePickup::Respawn, RespawnTime);
 }
 
