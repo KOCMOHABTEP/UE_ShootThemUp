8693541030ed09513da0c87abc6eff04d775e3ba
Dmitriy Smirnov <dsmirnov.online@yandex.ru>
2023-02-23 21:33:12 +0300

    GameModeBase spawning AI actors

--- Source/ShootThemUp/Private/STUGameModeBase.cpp
+++ Source/ShootThemUp/Private/STUGameModeBase.cpp
@@ -3,6 +3,7 @@
 
 #include "STUGameModeBase.h"
 
+#include "AIController.h"
 #include "STUBaseCharacter.h"
 #include "STUGameHUD.h"
 #include "STUPlayerController.h"
@@ -12,4 +13,34 @@ ASTUGameModeBase::ASTUGameModeBase()
 	DefaultPawnClass = ASTUBaseCharacter::StaticClass();
 	PlayerControllerClass = ASTUPlayerController::StaticClass();
 	HUDClass = ASTUGameHUD::StaticClass();
+}
+
+void ASTUGameModeBase::StartPlay()
+{
+	Super::StartPlay();
+	SpawnBots();
+}
+
+UClass* ASTUGameModeBase::GetDefaultPawnClassForController_Implementation(AController* InController)
+{
+	if (InController && InController->IsA<AAIController>())
+	{
+		return AIPawnClass;
+	}
+
+	return Super::GetDefaultPawnClassForController_Implementation(InController);
+}
+
+void ASTUGameModeBase::SpawnBots()
+{
+	if (!GetWorld()) return;
+
+	for (int32 i = 0; i< GameData.PlayersNum - 1; ++i)
+	{
+		FActorSpawnParameters SpawnInfo;
+		SpawnInfo.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+		const auto STUAIController = GetWorld()->SpawnActor<AAIController>(AIControllerClass, SpawnInfo);
+		RestartPlayer(STUAIController);
+	}
 }
